e0b29beac304d4c6196a2d574748129c2bf07249
Thirsty09 <nickifeajika@gmail.com>
2019-11-19 09:59:41 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# API Documentation
+## This is an API that facilitates creation of classrooms
+### Requirements
+- An API interactve client eg Postman or RapidAPI.
+
+### GET all data (Using Postman)
+1. visit the base URL/classes eg localhost:5000/class
+2. Set the API call to GET request (The default in postman is already GET)
+3. Hit **Send** and retrieve the data.
+
+### GET specific data using 'id'
+> **NOTE**: The only available retrieve parameter available is the 'id'
+1. visit the base URL/classes eg localhost:5000/class
+2. append the data id after a forward slash to the end eg localhost:5000/class/2, where 2 is the id
+3. Set the API call to GET request
+4. Hit send
+
+### POST data to the database, create a class
+1. visit the base URL/classes eg localhost:5000/class
+2. Go to **Headers** in the postman client and set **Content-Type** to **application/json**
+3. Go to **Body** and input the url and action in a JSON format with keys 'url' and 'action' eg 
+>{
+>    "url": "crowdclassroom.com"
+>    "action": "Created crowdclassroom"
+>}
+4. Hit **Send**
+#### NOTE
+- ALways use a double-quote when creating JSON objects for POST and PUT request
+- The id parameter is auto-incremented and should  not be tampered with.
+
+### PUT (Update Specific data)
+1. visit the base URL/classes eg localhost:5000/class
+2. append the data id to be updated after a forward slash to the end eg localhost:5000/class/2, where 2 is the id
+3. Input the new JSON data 
+4. Hit **Send**
+
+### DELETE (Delete Specific data)
+1. visit the base URL/classes eg localhost:5000/class
+2. append the data id to be deleted after a forward slash to the end eg localhost:5000/class/2, where 2 is the id
+3. Hit **Send**
---
+++ app.py
@@ -0,0 +1,95 @@
+from flask import Flask, render_template, redirect, url_for, jsonify, request
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#init app
+app = Flask(__name__)
+
+# Database config
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rest.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# Init db -- sqlalchemy
+db = SQLAlchemy(app)
+# Init ma -- Marshmallow
+ma = Marshmallow(app)
+
+
+# Crowdclassroom classes
+class Classroom(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String(128), unique=True, nullable=False)
+    action = db.Column(db.String(256))
+
+    def __init__(self, url, action):
+        self.url = url
+        self.action = action
+
+# The Schema
+class ClassSchema(ma.Schema):
+    class Meta:
+        fields = ('id', 'url', 'action')
+
+# Init schema
+class_schema = ClassSchema() # one object
+classes_schema =  ClassSchema(many=True) # Many Objects
+
+## Routes
+@app.route('/')
+def index():
+    return 'Api Tutorial'
+
+ # Create a Class   
+@app.route('/class', methods=['POST'])
+def create_class():
+    url = request.json['url']
+    action = request.json['action']
+
+    new_class = Classroom(url, action)
+
+    db.session.add(new_class)
+    db.session.commit()
+
+    return class_schema.jsonify(new_class)
+
+# Get All Classes
+@app.route('/class', methods=['GET'])
+def get_classes():
+    all_classes = Classroom.query.all()
+    result = classes_schema.dump(all_classes) # for an array only
+    return jsonify(result)
+
+# Get Single Classes
+@app.route('/class/<id>', methods=['GET'])
+def get_class(id):
+    single_class = Classroom.query.get(id) # fetching the particular class
+    return class_schema.jsonify(single_class)
+
+ # Update a Class   
+@app.route('/class/<id>', methods=['PUT'])
+def update_class(id):
+    single_class = Classroom.query.get(id)
+
+    url = request.json['url']
+    action = request.json['action']
+
+    # Constructing new product to subt to the DB in place of the old product with the given id
+    single_class.url = url
+    single_class.action = action
+
+    db.session.commit()
+
+    return class_schema.jsonify(new_class)
+
+# Delete Class
+@app.route('/class/<id>', methods=['DELETE'])
+def delete_class(id):
+    single_class = Classroom.query.get(id) # fetching the particular class
+    db.session.delete(single_class)
+    db.session.commit() # committing the delete, to take effect
+    return class_schema.jsonify(single_class)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
